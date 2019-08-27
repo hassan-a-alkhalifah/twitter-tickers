f4ed6cfaf8da3b511507573c5c185bcb1d301e6b
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-08-27 13:31:20 -0700

    change socketIOClient ref

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -110,8 +110,8 @@ class App extends Component {
   };
 
   componentDidMount() {
-    const socket = socketIOClient('http://localhost:3000/');
-
+    // const socket = socketIOClient('http://localhost:3000/');
+    const socket = socketIOClient('https://agile-plains-31778.herokuapp.com/');
     socket.on('connect', () => {
         console.log('Socket connected');
         socket.on('tweets', data => {
