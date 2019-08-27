0fc6b6f2c6b8243deec43fb197878d6c1b4947f9
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-08-27 15:38:36 -0700

    remove unessessary console.logs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -114,7 +114,6 @@ class App extends Component {
   };
 
   componentDidMount() {
-    // const socket = socketIOClient('http://localhost:3000/');
     const socket = socketIOClient('https://agile-plains-31778.herokuapp.com/');
     socket.on('connect', () => {
         console.log('Socket connected');
