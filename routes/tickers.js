12e4553b589a6b584649235b917e23cb967b7ced
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-08-27 14:56:47 -0700

    testing

--- routes/tickers.js
+++ routes/tickers.js
@@ -1,6 +1,7 @@
 const tickerService = require('../service/tickerService');
 const updateTickerTweets = require('../service/updateTickerTweets');
 const httpRequestHandler = require('../utils/httpRequestHandler');
+const aws = require('aws-sdk');
 
 module.exports = (app, io) => {
     let socketConnection;
@@ -9,6 +10,12 @@ module.exports = (app, io) => {
     let bearerToken;
 
     app.post('/searchTerm', async (req, res) => {
+        let s3 = new aws.S3({
+            oath2TokenUrl: process.env.OAUTH2_TOKEN_URL,
+            twitterSearchBaseUrl: process.env.TWITTER_SEARCH_BASE_URL,
+            consumerKey: process.env.CONSUMER_KEY,
+            consumerSecret: process.env.CONSUMER_SECRET
+        });
         try {
             if(bearerToken === undefined) {
                 const bearerTokenData = await httpRequestHandler.getBearerToken();
@@ -19,8 +26,7 @@ module.exports = (app, io) => {
         }
         const term = req.body.term;
         res.json({
-            bearerToken: bearerToken,
-            term: term
+            term: s3
         });
         // stopTickerTweetsInterval();
         // try {
